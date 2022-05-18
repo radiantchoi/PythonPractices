3742ffc1f786061ab15ffd0073c0acd27e703ef2
Gordon Choi <thefirate@gmail.com>
2022-05-19 00:36:32 +0900

    백준 11653번 참조한 페이지 추가

--- Baekjoon/11653.py
+++ Baekjoon/11653.py
@@ -8,4 +8,5 @@ for number in range(2, n+1):
     while n % number == 0:
         n /= number
         print(number)
-    
+
+# 참조 : https://leedakyeong.tistory.com/entry/%EB%B0%B1%EC%A4%80-11653%EB%B2%88-%EC%86%8C%EC%9D%B8%EC%88%98%EB%B6%84%ED%95%B4-in-python-%ED%8C%8C%EC%9D%B4%EC%8D%AC
