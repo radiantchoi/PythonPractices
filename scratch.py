865749f6ecb6b45005cf1aadd7ea16c8ba4e20d1
Gordon Choi <thefirate@gmail.com>
2022-06-09 15:33:55 +0900

    실험 - [0]은 True를 반환할까? 결론은 그렇다.

--- scratch.py
+++ scratch.py
@@ -1,7 +1,4 @@
-from math import comb
-
-i = int(input())
-
-for case in range(i):
-    n, m = map(int, input().split())
-    print(comb(m, n))
+if [0]:
+    print("True")
+else:
+    print("False")
