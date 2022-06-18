7eefd98eeb8e97242268fb333d35883fc855247e
Gordon Choi <thefirate@gmail.com>
2022-06-18 14:56:27 +0900

    LeetCode No.977 Squares of a Sorted Array

---
+++ LeetCode/977.py
@@ -0,0 +1,8 @@
+# LeetCode No.977 Squares of a Sorted Array
+# https://leetcode.com/problems/squares-of-a-sorted-array/
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        return sorted([x ** 2 for x in nums])
+
+# 느는 건 파이써닉함뿐인가
